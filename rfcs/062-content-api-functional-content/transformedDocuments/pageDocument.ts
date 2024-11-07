8ff4ff184886175f9d32415bf5b870859524df48
Raphaelle Cantin <r.cantin@wellcome.ac.uk>
2024-11-07 12:16:58 +0000

    Add transformed page and visual story documents

---
+++ rfcs/062-content-api-functional-content/transformedDocuments/pageDocument.ts
@@ -0,0 +1,20 @@
+// Proposal for transformed pageDocument.
+
+const pageDocument = {
+  id: "WwQHTSAAANBfDYXU",
+  uid: "opening-times",
+  display: {
+    type: "Page",
+    id: "WwQHTSAAANBfDYXU",
+    uid: "opening-times",
+    title: "Opening times",
+    caption: "We are a free museum and library connecting science, medicine, life and art. You’ll find exhibitions, events and a library in our beautiful building in central London.",
+    siteSection: "visit-us", // Should this be the uid or the readable name? Is it a label or for links? Probably both.
+  },
+  query: {
+    title: "Opening times",
+    id: "", // ?
+    caption: "We are a free museum and library connecting science, medicine, life and art. You’ll find exhibitions, events and a library in our beautiful building in central London.",
+    body: "Lorem ipsum dolor sit amet"
+  },
+};
---
+++ rfcs/062-content-api-functional-content/transformedDocuments/visualStoryDocument.ts
@@ -0,0 +1,20 @@
+// Proposal for transformed visualStoryDocument.
+
+const visualStoryDocument = {
+  id: "ZdTCPREAACEA3zK4",
+  uid: "jason-and-the-adventure-of-254-visual-story",
+  display: {
+    type: "Visual story",
+    id: "ZdTCPREAACEA3zK4", // ?
+    uid: "jason-and-the-adventure-of-254-visual-story",
+    title: "Jason and the adventure of 254 visual story",
+    standfirst: "", // Or Caption?
+  },
+  query: {
+    type: "Visual story",
+    id: "", // ?
+    title: "Jason and the adventure of 254 visual story",
+    standfirst: "",
+    caption: ""
+  },
+};
